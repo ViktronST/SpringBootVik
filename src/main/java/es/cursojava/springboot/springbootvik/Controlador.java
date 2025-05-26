9f74122f4fc19575351e0f864345a987882a8c7e
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-05-26 20:46:43 +0200

    Tercer Push

--- src/main/java/es/cursojava/springboot/springbootvik/Controlador.java
+++ src/main/java/es/cursojava/springboot/springbootvik/Controlador.java
@@ -5,14 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
 
 @Controller
 public class Controlador {
 
     @RequestMapping("/inicio")
-    public String metodo1(Model model, HttpServletRequest request, LocalDate fecha) {
+    public String metodo1(Model model, HttpServletRequest request) {
         System.out.println("Mi primer controlador de Spring Boot");
+        model.addAttribute("mensaje", "Viktor Stoyanov");
         return "Hola";
     }
+
 }
