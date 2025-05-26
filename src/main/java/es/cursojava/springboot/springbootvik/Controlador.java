674ace535d7d22282b30b4e788998175bccd8063
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-05-26 20:50:17 +0200

    Cuarto Push

--- src/main/java/es/cursojava/springboot/springbootvik/Controlador.java
+++ src/main/java/es/cursojava/springboot/springbootvik/Controlador.java
@@ -9,10 +9,13 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class Controlador {
 
+    // http://localhost:9091/inicio?param1=1
     @RequestMapping("/inicio")
     public String metodo1(Model model, HttpServletRequest request) {
         System.out.println("Mi primer controlador de Spring Boot");
+        String valor = request.getParameter("param1");
         model.addAttribute("mensaje", "Viktor Stoyanov");
+        model.addAttribute("param_valor", valor);
         return "Hola";
     }
 
